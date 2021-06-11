fc76475da9dd158025dd095660812f3ab12e23b2
Allen Cao <allencao@berkeley.edu>
2021-06-10 17:38:10 -0700

    made js script of alert, prompt

---
+++ interactions.js
@@ -0,0 +1,22 @@
+/* INTERACTIONS */
+/** alert
+ * Shows a message and waits for the user to press “OK”.
+ * Returns true if OK pressed, returns false if esc/cancel pressed.
+ */
+
+ result = alert("Hello there!");
+
+ /** prompt
+  * It shows a modal window with a text message, an input field for the visitor, and the buttons OK/cancel.
+  * result = prompt(title, [default]);
+  * 
+  * title = The text to show the visitor
+  * 
+  * default = An optional second parameter, the initial value for the input field.
+  * Return value: if press OK, then returns user input; if cancel then returns null.
+  * 
+ */
+result = prompt("What is the 320th digit of pi");
+bruh = prompt("What is up", "not bad");
+
+ 
